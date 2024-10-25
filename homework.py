d5ac0fb576351cd9a11597c08cb0760a327b1ffe
hvz1961 <vzkhametov@gmail.com>
2024-10-25 18:39:07 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -18,3 +18,75 @@
 # было "постоянное состояние" между запусками программы.
 
 class Animal():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def make_sound(self):
+        pass
+    def make_eat(self):
+        print(f'{self.name} кушает')
+
+class Bird(Animal):
+    def make_sound(self):
+        print('чирик чирик')
+
+class Mammal(Animal):
+    def make_sound(self):
+        print('гав гав')
+
+class Reptile(Animal):
+    def make_sound(self):
+        print('Шшшшш')
+def animals_sound(animals):
+    for animal in animals:
+        animal.make_sound()
+
+
+class Zoo():
+    def __init__(self):
+        self.animals = []
+        self.staff = []
+
+    def add_animal(self, animal):
+        self.animals.append(animal)
+        print(f'животное {animal.name} добавлено в зоопарк')
+    def add_staff(self, new_staff):
+        self.staff.append(new_staff)
+        print(f'сотрудник {new_staff.name} добавлен в зоопарк')
+
+class ZooKeeper():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def feed_animal(self, animal):
+        print(f'сотрудник кормит {animal.name}')
+
+
+class ZooVeterinarian():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def heal_animal(self, animal):
+        print(f'ветеринар лечит {animal.name}')
+
+bird1 = Bird('Птица', 1)
+mammal1 = Mammal('Собака', 3)
+reptile1 = Reptile('Ящерица', 10)
+
+zoo = Zoo()
+
+keeper = ZooKeeper('Николай', 43)
+veterinarian = ZooVeterinarian('Анатолий', 32)
+
+zoo.add_animal(bird1)
+zoo.add_animal(mammal1)
+zoo.add_animal(reptile1)
+
+zoo.add_staff(keeper)
+zoo.add_staff(veterinarian)
+
+animals_sound(zoo.animals)
+
+keeper.feed_animal(mammal1)
+veterinarian.heal_animal(bird1)
+
